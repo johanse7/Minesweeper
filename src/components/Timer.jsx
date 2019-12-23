b8adaf4fc933bfb69e14548e88ef2664e60e461e
Sebastian Rodriguez Sanchez <xcbsrodr@intcomex.com>
2019-12-23 10:59:25 -0500

    add  Timer Logic

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import timer from '../assets/static/timer.png';
 import '../assets/styles/components/Timer.scss';
 
 const Timer = (props) => {
+
+  const [timerState, setTimerState] = useState({
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTimerState({
+        seconds: timerState.seconds === 59 ? 0 : timerState.seconds + 1,
+        minutes: timerState.minutes + parseInt(((timerState.seconds + 1) / 60)),
+      });
+
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, [timerState])
+
+
+
+
   return (
     <div className="content-timer">
       <img src={timer} alt="timer" />
-      <span>10:05</span>
+      <span>{`${timerState.minutes < 10 ? '0' : ''}${timerState.minutes}:${timerState.seconds < 10 ? '0' : ''}${timerState.seconds}`}</span>
     </div>
   );
 };
